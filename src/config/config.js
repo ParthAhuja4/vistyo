e7783e1c70c1e4ba0bb7afc1babd043ab0d23efa
Parth Ahuja <195557893+ParthAhuja4@users.noreply.github.com>
2025-07-30 12:11:31 +0530

    env change

--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,33 @@
 const config = {
-  appwriteUrl: String(import.meta.env.APPWRITE_ENDPOINT),
-  appwriteProjectId: String(import.meta.env.APPWRITE_PROJECT_ID),
-  appwriteDatabaseId: String(import.meta.env.APPWRITE_DATABASE_ID),
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 
   usersMetadataCollectionId: String(
-    import.meta.env.USERS_METADATA_COLLECTION_ID,
+    import.meta.env.VITE_USERS_METADATA_COLLECTION_ID,
   ),
-  rolesCollectionId: String(import.meta.env.ROLES_COLLECTION_ID),
-  roleKeywordsCollectionId: String(import.meta.env.ROLE_KEYWORDS_COLLECTION_ID),
+  rolesCollectionId: String(import.meta.env.VITE_ROLES_COLLECTION_ID),
+  roleKeywordsCollectionId: String(
+    import.meta.env.VITE_ROLE_KEYWORDS_COLLECTION_ID,
+  ),
   channelFiltersCollectionId: String(
-    import.meta.env.CHANNEL_FILTERS_COLLECTION_ID,
+    import.meta.env.VITE_CHANNEL_FILTERS_COLLECTION_ID,
   ),
-  searchesCollectionId: String(import.meta.env.SEARCHES_COLLECTION_ID),
-  historyCollectionId: String(import.meta.env.HISTORY_COLLECTION_ID),
+  searchesCollectionId: String(import.meta.env.VITE_SEARCHES_COLLECTION_ID),
+  historyCollectionId: String(import.meta.env.VITE_HISTORY_COLLECTION_ID),
 
   appwriteFunctionCreateSession: String(
-    import.meta.env.APPWRITE_FUNCTION_CREATE_SESSION,
+    import.meta.env.VITE_APPWRITE_FUNCTION_CREATE_SESSION,
   ),
   appwriteFunctionStripeWebhook: String(
-    import.meta.env.APPWRITE_FUNCTION_STRIPE_WEBHOOK,
+    import.meta.env.VITE_APPWRITE_FUNCTION_STRIPE_WEBHOOK,
   ),
 
-  stripePublicKey: String(import.meta.env.STRIPE_PUBLIC_KEY),
+  stripePublicKey: String(import.meta.env.VITE_STRIPE_PUBLIC_KEY),
 
-  ytApiKey: String(import.meta.env.YOUTUBE_API_KEY),
+  ytApiKey: String(import.meta.env.VITE_YOUTUBE_API_KEY),
 
-  aiApiKey: String(import.meta.env.COHERE_AI_API_KEY),
+  aiApiKey: String(import.meta.env.VITE_COHERE_AI_API_KEY),
 };
 
 export default config;
